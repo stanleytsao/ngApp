fe887204149b48e99803459be14a378c2872498b
Stanley Tsao <stsao002@gmail.com>
2019-10-10 13:42:21 -0400

    Rename Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ProductAddComponent } from './product-add/product-add.component';
-import { ProductEditComponent } from './product-edit/product-edit.component';
-import { ProductGetComponent } from './product-get/product-get.component';
+import { CreateComponent } from './components/create/create.component';
+import { EditComponent } from './components/edit/edit.component';
+import { IndexComponent } from './components/index/index.component';
 
 const routes: Routes = [
   {
-    path: 'product/create',
-    component: ProductAddComponent
+    path: 'todos/create',
+    component: CreateComponent
   },
   {
     path: 'edit/:id',
-    component: ProductEditComponent
+    component: EditComponent
   },
   {
-    path: 'products',
-    component: ProductGetComponent
+    path: 'todos',
+    component: IndexComponent
   }
 ];
 
